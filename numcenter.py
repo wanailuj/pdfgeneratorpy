f189e4f2b45d92ae65d136ed90e88d1843b69d09
Juliana Moreira <jjuabra@gmail.com>
2023-07-07 12:14:27 -0300

    Primeiro commit

---
+++ numcenter.py
@@ -0,0 +1,42 @@
+from fpdf import FPDF
+from jinja2 import Environment, FileSystemLoader
+
+# Defina o caminho para o arquivo HTML
+arquivo_html = 'receita.html'
+
+# Configurações do PDF
+opcoes_pdf = {
+    'page-size': 'Letter',
+    'margin-top': '0mm',
+    'margin-right': '0mm',
+    'margin-bottom': '0mm',
+    'margin-left': '0mm',
+}
+
+# Carregar o conteúdo do arquivo HTML usando o Jinja2
+env = Environment(loader=FileSystemLoader('.'))
+template = env.get_template(arquivo_html)
+html_content = template.render()
+
+# Criar a classe do PDF personalizada com a função de cabeçalho e rodapé
+class MyPDF(FPDF):
+    def header(self):
+        pass
+
+    def footer(self):
+        self.set_y(-10)
+        self.set_font('Helvetica', 'I', 8)
+        self.cell(0, 10, f"Page {self.page_no()}/{{nb}}", align="C")
+
+# Inicializar o objeto PDF personalizado
+pdf = MyPDF()
+pdf.set_auto_page_break(auto=True, margin=0)
+pdf.add_page()
+
+# Adicionar o conteúdo HTML ao PDF
+pdf.write_html(html_content)
+
+# Salvar o PDF com o conteúdo e as páginas numeradas
+pdf.output('output_com_paginas.pdf')
+
+print("PDF gerado com sucesso.")
